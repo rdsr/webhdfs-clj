7d239b6b8a57d8b30fe0db3c093e7461a83b1027
Ratandeep S. Ratti <rratti@linkedin.com>
2014-10-28 11:09:08 +0530

    using clj-http instead of http-kit, since clj-http provides debugging info

--- project.clj
+++ project.clj
@@ -3,6 +3,5 @@
   :description "Clojure client library for WebHDFS Rest API"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.2.5"]
-                 [http-kit "2.1.16"]
-                 [org.apache.hadoop/hadoop-auth "2.3.0"
-                  :exclusions [[org.apache.httpcomponents/httpclient]]]])
+                 [clj-http "1.0.0" :exclusions [cheshire crouton org.clojure/tools.reader]]
+                 [org.apache.hadoop/hadoop-auth "2.3.0" :exclusions [[org.apache.httpcomponents/httpclient]]]])
--- src/webhdfs_clj/core.clj
+++ src/webhdfs_clj/core.clj
@@ -2,13 +2,15 @@
   (:require [clojure.data.json :as json]
             [clojure.java.io :as io]
             [clojure.string :as s]
-            [org.httpkit.client :as http])
+            [clj-http.client :as http])
   (:import [java.net URL HttpURLConnection]
            [java.io StringWriter]
            [org.apache.hadoop.security.authentication.client 
-            AuthenticatedURL AuthenticatedURL$Token])) 
+            AuthenticatedURL AuthenticatedURL$Token]))
 
-(defn authenticate [url]
+(def conf {:host "eat1-euchrenn01.grid.linkedin.com" :port 50070})
+
+(defn- authenticate [url]
   (let [url (if (instance? URL url) url (URL. url)) 
         token (AuthenticatedURL$Token.)
         connection (.openConnection (AuthenticatedURL.) url token)
@@ -20,9 +22,6 @@
         (Exception. 
           (str "Error when authenticating to url:" url ". Response code: " response-code)))))) 
                
-;;(authenticate (URL. (str "http://eat1-euchrenn01.grid.linkedin.com:50070/webhdfs/v1/" 
-  ;;                       "data/tracking/EndorsementsSuggestionImpressionEvent/daily/2014/10/06/part-00000.avro?op=OPEN")))
-
 (defn- expiration-time [cookie]
   (java.util.Date.
     (Long/valueOf 
@@ -33,44 +32,47 @@
                     (.split cookie "&"))) 
           "=")))))
 
-(expiration-time "\"u=rratti&p=***REMOVED***&t=kerberos&e=1414469737167&s=xcIyxNley7n0l73qwBo7pjXek0M=\""
-)
-  
 (defn- expired? [cookie]
-  (<= (compare (expiration-time cookie)
-               (java.util.Date.))
-      0))
+  (let [cal (java.util.Calendar/getInstance)]
+    ;; expire a few seconds early
+    (.add cal java.util.Calendar/SECOND 10)
+    (<= (compare (expiration-time cookie)
+                 (.getTime cal))
+        0)))
 
-(expired? "\"u=rratti&p=***REMOVED***&t=kerberos&e=1414469737167&s=xcIyxNley7n0l73qwBo7pjXek0M=\"")
-  
 (def ^:private cookie (atom nil))
 
-(defn- get-cookie! [url]
-  (if (or (nil? @cookie) (cookie-expired? @cookie))
-    (swap! cookie (fn [_] (authenticate url)))
-    @cookie))
-  
-(def conf {:host ""
-           :port 50070})
-
-(defn- url [path parameters]
+(defn- base-url []
   (let [{:keys [host port]} conf]
-    (URL. (str "http://" host ":" port "/webhdfs/v1/" path "?" 
-               (s/join "&" (map (fn [[k v]] (str k "=" v)) parameters))))))
+    (str "http://" host ":" port "/webhdfs/v1")))
 
-(defn http-put [path parameters]
-  ;(http/put
-  )
+(defn- get-cookie! []
+  (if (or (nil? @cookie) (expired? @cookie))
+    (let [url (str (base-url) "/tmp?op=GETFILESTATUS")]
+      (swap! cookie (fn [_] (authenticate url))))
+    (str "hadoop.auth=" @cookie)))
 
-(defn throw-exception [json-data]
+(get-cookie!)
+  
+(defn http-put [path query-params]
+  (let [{:keys [status headers body error]}
+         (http/put (str (base-url) path)
+                   {:debug true
+                    :query-params query-params
+                    :headers {:cookie (get-cookie!)}})]
+    [status headers body error]))
+
+(defn- throw-exception [json-data]
   (let [{:keys [javaClassName message]} (:RemoteException json-data)]
     (throw 
       (clojure.lang.Reflector/invokeConstructor 
         (resolve (symbol javaClassName)) 
         (to-array [message])))))
 
-(defn remote-exception? [response]
+(defn- remote-exception? [response]
   (contains? response :RemoteException))
+
+(http-put "/tmp/abc" {:op "MKDIRS" :permissions "755"})
 
 (defn mkdir [path & [permissions]]
   (let [response (http-put path {:op :MKDIRS :permissions permissions})]
@@ -78,6 +80,4 @@
       (throw-exception response)
       (:boolean response))))
 
-(defn get-file-status [path]
-  (json/read-str (http/get (url path))))
         
